c5e1f5e78424e4df802f2ab8196306e9b96d7859
Vladimir Ilic <v.ilic@sks365.com>
2023-05-17 18:24:12 +0200

    First unit tests with Vitest by testing vanilla JavaScript imported fnc

---
+++ tests/unit/playground.test.js
@@ -0,0 +1,29 @@
+import { describe, it, expect } from 'vitest'
+
+import { evenOrOdd } from '@/playground.js'
+
+describe('basic math', () => {
+  it('adds two numbers', () => {
+    expect(1 + 1).toBe(2)
+  })
+
+  describe('evenOrOdd', () => {
+    describe('when number is even', () => {
+      it('indicates the number is even', () => {
+        expect(evenOrOdd(4)).toBe('Even')
+      })
+    })
+
+    describe('wen number is odd', () => {
+      it('indicates the number is odd', () => {
+        expect(evenOrOdd(7)).toBe('Odd')
+      })
+    })
+  })
+})
+
+describe('another top level describe in file', () => {
+  it('subtracting two numbers', () => {
+    expect(1 - 1).toBe(0)
+  })
+})
