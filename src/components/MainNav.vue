ee03d5fcab9456b43f62f791f97b1225b3837ea1
Vladimir Ilic <v.ilic@sks365.com>
2023-06-14 14:38:56 +0200

    End of Section 9

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,19 +2,44 @@
   <header class="w-full text-sm">
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="ml-9 h-full first:ml-0" v-for="menuItem in menuItems" :key="menuItem">
+              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="ml-auto flex h-full items-center">
+          <ProfileImage v-if="isLoggedIn" />
+          <ActionButton v-else @click="loginUser" />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from '@/components/ActionButton.vue'
+import ProfileImage from '@/components/ProfileImage.vue'
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: 'Bobo careers',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Bobo corp', 'How we hire', 'Students', 'Jobs'],
+      isLoggedIn: false,
     }
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true
+    },
   },
 }
 </script>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,13 +1,77 @@
 import { render, screen } from '@testing-library/vue'
+import userEvent from '@testing-library/user-event'
 
 // Component is the default export of the file
 import MainNav from '@/components/MainNav.vue'
 
 describe('MainNav', () => {
-  it('displayes company name', () => {
+  it('displays company name', () => {
     render(MainNav)
     screen.debug()
     const companyName = screen.getByText('Bobo careers')
+    expect(companyName).toBeInTheDocument()
+  })
+
+  /**
+   * The expected outcome might be hardcoded
+   * If we would follow TDD approach this would be the test we would write
+   */
+  it('displays manu items for navigation', () => {
+    render(MainNav)
+    // screen.getByRole('listitem')
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent)
+    console.log(navigationMenuTexts)
+    // expect(navigationMenuTexts).toBe()
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Bobo corp',
+      'How we hire',
+      'Students',
+      'Jobs',
+    ])
+  })
+
+  describe('when the user logs in', () => {
+    it('displayes user profile picture', async () => {
+      // write all test assertions
+      render(MainNav)
+
+      // screen.getByRole('img')
+      let profileImage = screen.queryByRole('img', {
+        name: /user profile image/i,
+      })
+      expect(profileImage).not.toBeInTheDocument()
+
+      const loginButton = screen.getByRole('button', {
+        name: /sign in/i,
+      })
+
+      await userEvent.click(loginButton)
+
+      // By this point our ProfileImage should exist
+      profileImage = screen.getByRole('img', {
+        name: /user profile image/i,
+      })
+      expect(profileImage).toBeInTheDocument()
+    })
+  })
+})
+
+/**
+ * This is not good appraoch since it goes too deep into the implementation details
+ */
+describe('MainNav with second argument', () => {
+  it('displayes company name', () => {
+    render(MainNav, {
+      data() {
+        return {
+          company: 'Some other name',
+        }
+      },
+    })
+    const companyName = screen.getByText('Some other name')
     expect(companyName).toBeInTheDocument()
   })
 })
