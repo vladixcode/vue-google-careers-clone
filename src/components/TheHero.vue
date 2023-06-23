1e4d91ebcd4bd0359905476e9574f1e64e009eee
Vladimir Ilic <v.ilic@sks365.com>
2023-06-23 13:45:42 +0200

    Section 13 - Add an nice Vue logo image to the Hero component

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -7,7 +7,13 @@
           <the-headline />
           <job-search-form />
         </div>
-        <div class="col-span-5 col-start-7"></div>
+        <div class="col-span-5 col-start-7 justify-self-center">
+          <img
+            class="h-80 w-80 object-contain"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+            alt=""
+          />
+        </div>
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
