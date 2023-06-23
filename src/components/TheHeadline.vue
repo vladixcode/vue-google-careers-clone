f3bdcdad0115d3ecbf947873511bd10c1739afea
Vladimir Ilic <v.ilic@sks365.com>
2023-06-23 11:39:25 +0200

    End of Section 12

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import nextElementInList from '@/utils/nextElementInList'
+
 export default {
   name: 'TheHeadline',
   data() {
@@ -35,10 +37,7 @@ export default {
     changeTitle() {
       this.interval = setInterval(() => {
         const actions = ['Build', 'Create', 'Design', 'Code']
-        const currentActionIndex = actions.indexOf(this.action)
-        const nextActionIndex = (currentActionIndex + 1) % actions.length
-        const nextAction = actions[nextActionIndex]
-        this.action = nextAction
+        this.action = nextElementInList(actions, this.action)
       }, 3000)
     },
   },
