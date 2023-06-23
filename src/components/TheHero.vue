4f65b689d3af96bdd325501062c20a7875ef4e64
Vladimir Ilic <v.ilic@sks365.com>
2023-06-23 12:51:40 +0200

    Section 12 - Nice, lean, elegant and modern design for the form

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,13 +1,14 @@
 <template>
   <main>
-    <section class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
+    <section class="flex h-screen flex-col pb-20 pt-10">
       <div class="grid grid-cols-12">
-        <div class="col-span-1 col-start-1 border border-blue-700">1 column</div>
-        <div class="col-span-5 col-start-2 border border-blue-700">
+        <div class="col-span-1 col-start-1"></div>
+        <div class="col-span-5 col-start-2">
           <the-headline />
+          <job-search-form />
         </div>
-        <div class="col-span-5 col-start-7 border border-blue-700">5 column</div>
-        <div class="col-span-1 col-start-12 border border-blue-700">1 column</div>
+        <div class="col-span-5 col-start-7"></div>
+        <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
   </main>
@@ -15,11 +16,13 @@
 
 <script>
 import TheHeadline from '@/components/TheHeadline.vue'
+import JobSearchForm from '@/components/JobSearchForm.vue'
 
 export default {
   name: 'TheHero',
   components: {
     TheHeadline,
+    JobSearchForm,
   },
 }
 </script>
