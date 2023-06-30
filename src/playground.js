cf7539cfe0e57d15c9557b34be514cc66572716d
Vladimir Ilic <v.ilic@sks365.com>
2023-06-30 18:24:20 +0200

    Section 18 - Setup JSON Server & make fetch request to GET /jobs & render the results on UI

--- src/playground.js
+++ src/playground.js
@@ -5,3 +5,18 @@ export const evenOrOdd = (number) => {
     return 'Odd'
   }
 }
+
+// const axios = require('axios')
+
+// const url = 'http://localhost:3000/jobs'
+
+// const fetchJobsThenSyntax = () => {
+//   axios.get(url).then((response) => {
+//     console.log(response.data)
+//   })
+// }
+
+// const fetchJobsNewSyntax = async () => {
+//   const response = await axios.get(url)
+//   console.log(response.data)
+// }
