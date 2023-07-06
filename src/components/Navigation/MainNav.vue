0953adbdd07558518e7af5551709791e1684f82c
Vladimir Ilic <vladimir.ilic101@gmail.com>
2023-07-06 10:27:36 +0200

    End of Section 20 - Testing components interaction with Pinia store and use mapActions & mapState to connect components to Pinia global store

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -26,9 +26,14 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'pinia'
+
+import { useUserStore } from '@/stores/user'
+
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import ProfileImage from '@/components/Navigation/ProfileImage.vue'
 import TheSubnav from '@/components/Navigation/TheSubnav.vue'
+
 export default {
   name: 'MainNav',
   components: {
@@ -46,10 +51,10 @@ export default {
         { text: 'Students', url: '/' },
         { text: 'Jobs', url: '/jobs/results' },
       ],
-      isLoggedIn: false,
     }
   },
   computed: {
+    ...mapState(useUserStore, ['isLoggedIn']),
     headerHeightClass() {
       return {
         'h-16': !this.isLoggedIn,
@@ -58,9 +63,7 @@ export default {
     },
   },
   methods: {
-    loginUser() {
-      this.isLoggedIn = true
-    },
+    ...mapActions(useUserStore, ['loginUser']),
   },
 }
 </script>
