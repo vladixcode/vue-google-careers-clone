d0eb6655ac451f54845570b74bd6998a89ae2c5b
Vladimir Ilic <vladimir.ilic101@gmail.com>
2023-07-10 15:21:16 +0200

    Section 22 - Add Collapsible Accordion component & Slots components introduction

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4">
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+
+      <collapsible-accordion>Hello, custom content</collapsible-accordion>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from '@/components/Shared/ActionButton.vue'
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+
 export default {
   name: 'JobFiltersSidebar',
+  components: {
+    ActionButton,
+    CollapsibleAccordion,
+  },
 }
 </script>
