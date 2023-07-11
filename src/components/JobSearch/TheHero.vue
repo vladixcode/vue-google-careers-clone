d17a8b782cc52b4e341376416580fa6c2cf70446
Vladimir Ilic <vladimir.ilic101@gmail.com>
2023-07-11 16:52:06 +0200

    Section 24 - Add new SpotLight component with Scoped Slots

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pb-20 pt-10">
+  <main class="flex h-screen flex-col">
+    <section class="pb-20 pt-10">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"></div>
         <div class="col-span-5 col-start-2">
@@ -17,18 +17,39 @@
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
+
+    <spot-light class="flex flex-row justify-center pb-16">
+      <template #default="slotProps">
+        <router-link
+          to="/jobs/results"
+          class="mx-5 flex h-96 w-72 flex-col overflow-hidden rounded-lg border bg-brand-gray-2"
+        >
+          <img :src="slotProps.spotlight.img" class="object-contain" />
+
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">{{ slotProps.spotlight.title }}</h3>
+            <p class="mt-3 text-sm">{{ slotProps.spotlight.description }}</p>
+          </div>
+          <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-brand-blue-1">
+            See jobs
+          </router-link>
+        </router-link>
+      </template>
+    </spot-light>
   </main>
 </template>
 
 <script>
 import TheHeadline from '@/components/JobSearch/TheHeadline.vue'
 import JobSearchForm from '@/components/JobSearch/JobSearchForm.vue'
+import SpotLight from './SpotLight.vue'
 
 export default {
   name: 'TheHero',
   components: {
     TheHeadline,
     JobSearchForm,
+    SpotLight,
   },
 }
 </script>
