fc4a4660e3b2b7a46b700fd40eea30b184836233
Vladimir Ilic <vladimir.ilic101@gmail.com>
2023-07-11 16:56:52 +0200

    Section 24 - Add multiple props on Slots & use ES6 destructuring in the Parent component to simplify dynamic code to pass to the slot

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -19,16 +19,16 @@
     </section>
 
     <spot-light class="flex flex-row justify-center pb-16">
-      <template #default="slotProps">
+      <template #default="{ img, title, description }">
         <router-link
           to="/jobs/results"
           class="mx-5 flex h-96 w-72 flex-col overflow-hidden rounded-lg border bg-brand-gray-2"
         >
-          <img :src="slotProps.spotlight.img" class="object-contain" />
+          <img :src="img" class="object-contain" />
 
           <div class="mt-3 h-48 px-6 py-4">
-            <h3 class="text-lg font-medium">{{ slotProps.spotlight.title }}</h3>
-            <p class="mt-3 text-sm">{{ slotProps.spotlight.description }}</p>
+            <h3 class="text-lg font-medium">{{ title }}</h3>
+            <p class="mt-3 text-sm">{{ description }}</p>
           </div>
           <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-brand-blue-1">
             See jobs
