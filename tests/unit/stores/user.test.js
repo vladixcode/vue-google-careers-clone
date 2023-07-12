bbeaee7b3b05b4d4657a971c20ecd74c8dac848f
Vladimir Ilic <vladimir.ilic101@gmail.com>
2023-07-12 17:01:51 +0200

    Section 25 - TDD create test for mutating selected organizations action

--- tests/unit/stores/user.test.js
+++ tests/unit/stores/user.test.js
@@ -25,3 +25,11 @@ describe('user store actions', () => {
     expect(store.isLoggedIn).toBe(true)
   })
 })
+
+describe('ADD_SELECTED_ORGANIZATIONS', () => {
+  it('updates organizations the user has chosen to filter jobs by', () => {
+    const store = useUserStore()
+    store.ADD_SELECTED_ORGANIZATIONS(['org1', 'org2'])
+    expect(store.slectedOrganizations).toEqual(['org1', 'org2'])
+  })
+})
