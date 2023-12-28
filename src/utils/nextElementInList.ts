1224b1eaea652f7f44194bad918f79f8f109eadb
Vladimir Ilic <vladimir.ilic101@gmail.com>
2023-12-28 14:55:39 +0100

    Section 32 - Update Utils files to TypeScript

--- src/utils/nextElementInList.ts
+++ src/utils/nextElementInList.ts
@@ -1,4 +1,4 @@
-const nextElementInList = (list, currentValue) => {
+const nextElementInList = <T>(list: T[], currentValue: T) => {
   const currentValueIndex = list.indexOf(currentValue)
   const nextValueIndex = (currentValueIndex + 1) % list.length
   return list[nextValueIndex]
