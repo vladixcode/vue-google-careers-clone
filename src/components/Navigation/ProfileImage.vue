704d1e77fc97bee0da7881479e7756bed317ad57
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-01-16 10:28:48 +0100

    Section 33 - Converting simple ProfileImage Options API component to Composition API with TypeScript

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -1,25 +1,10 @@
 <template>
-  <img :src="linkImage" alt="User profile image" class="h-full rounded-3xl object-contain" />
+  <img :src="imageLink" alt="User profile image" class="h-full rounded-3xl object-contain" />
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue'
 
-interface Data {
-  linkImage: string
-}
-
-export default defineComponent({
-  name: 'ProfileImage',
-  data(): Data {
-    return {
-      linkImage:
-        'https://i.fbcd.co/products/original/9847a67d09a39d0ef02f4cacc70490cdbe8cae2a1f7c9a2e5bf23e9a126137ec.jpg',
-    }
-  },
-  methods: {
-    changeImageLink() {
-      this.linkImage = 'some other link'
-    },
-  },
-})
+const imageLink = ref(
+  'https://i.fbcd.co/products/original/9847a67d09a39d0ef02f4cacc70490cdbe8cae2a1f7c9a2e5bf23e9a126137ec.jpg',
+)
 </script>
