12c65b78624e3779af70cfd7bea7e7a8954b3ce8
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-01-16 13:39:12 +0100

    Section 33 - Annotating Event Handlers in Composition API and TypeScript

--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -8,23 +8,18 @@
   />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'TextInput',
-  props: {
-    modelValue: {
-      type: String,
-      required: true,
-    },
-  },
-  emits: ['update:modelValue'],
-  methods: {
-    handleInput($event: Event) {
-      const target = $event.target as HTMLInputElement
-      this.$emit('update:modelValue', target.value)
-    },
+<script lang="ts" setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
   },
 })
+
+const emit = defineEmits(['update:modelValue'])
+
+const handleInput = ($event: Event) => {
+  const target = $event.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
 </script>
