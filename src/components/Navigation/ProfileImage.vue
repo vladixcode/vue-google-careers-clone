94ab73f7fabef58f7186f1a13076c79af788e129
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-01-16 10:25:07 +0100

    Section 33 - TypeScript with Options API Vue component

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -1,14 +1,25 @@
 <template>
   <img :src="linkImage" alt="User profile image" class="h-full rounded-3xl object-contain" />
 </template>
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+interface Data {
+  linkImage: string
+}
+
+export default defineComponent({
   name: 'ProfileImage',
-  data() {
+  data(): Data {
     return {
       linkImage:
         'https://i.fbcd.co/products/original/9847a67d09a39d0ef02f4cacc70490cdbe8cae2a1f7c9a2e5bf23e9a126137ec.jpg',
     }
   },
-}
+  methods: {
+    changeImageLink() {
+      this.linkImage = 'some other link'
+    },
+  },
+})
 </script>
