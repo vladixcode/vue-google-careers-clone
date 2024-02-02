a63c920170a7f55089b75f9c7540d3f4d4b1c6eb
Vladimir Ilic <vladimir.ilic101@gmail.com>
2024-02-02 18:12:18 +0100

    Section 36 - Alternative computed syntax with set and get in order to apply single source of truth

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="mt-2">
     <input
+      v-model="skillSearchTerm"
       type="text"
       placeholder="Computer programming"
       class="h-12 w-full rounded border border-brand-gray-1 p-3 text-base shadow-gray"
@@ -9,7 +10,17 @@
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
 import { useUserStore } from '@/stores/user'
 
 const userStore = useUserStore()
+
+const skillSearchTerm = computed({
+  get() {
+    return userStore.skillsSearchTerm
+  },
+  set(term) {
+    userStore.UPDATE_SKILLS_SEARCH_TERM(term)
+  },
+})
 </script>
